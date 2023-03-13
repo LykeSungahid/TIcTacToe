409e0d80592b3235e1dde25db65b80725275e34a
LykeSungahid <17100098@usc.edu.ph>
2023-03-13 23:09:06 +0800

    TABANG

--- TicTacToe/MainPage.xaml.cs
+++ TicTacToe/MainPage.xaml.cs
@@ -10,9 +10,113 @@ namespace TicTacToe
 {
     public partial class MainPage : ContentPage
     {
+        
+        int currentState = 1;
+
+        List<int> x = new List<int>();
+        List<int> o = new List<int>();
+
+
+
         public MainPage()
         {
             InitializeComponent();
+
+        }
+
+        void OnSelectBox(object sender, EventArgs e)
+        {
+
+
+            int num;
+            
+               
+            
+            if ( currentState == 1)
+            {
+                Button button = (Button)sender;
+                string pressed = button.Text;
+                button.Text = "X";
+                button.IsEnabled = false;
+                currentState = 2;
+                this.OText.Text = "O Player Turn";
+                this.XText.Text = "X Player Wait";
+                int.TryParse( button.ClassId, out num);
+                x.Add(num);
+                if (x.Count >= 3)
+                {
+                    checkGame();
+                }
+
+
+            }
+            else
+            {
+                
+                Button button = (Button)sender;
+                string pressed = button.Text;
+                button.Text = "0";
+                button.IsEnabled = false;
+                currentState = 1;
+                this.OText.Text = "O Player Wait";
+                this.XText.Text = "X Player Turn";
+                int.TryParse(button.ClassId, out num);
+                o.Add(num);
+                if (o.Count >= 3)
+                {
+                    checkGame();
+                }
+            }
+        
+        }
+
+        void checkGame()
+        {
+            if ((x.Contains(1) && x.Contains(2) && x.Contains(3)) ||
+                (x.Contains(4) && x.Contains(5) && x.Contains(6)) ||
+                (x.Contains(7) && x.Contains(8) && x.Contains(9)) ||
+                (x.Contains(1) && x.Contains(4) && x.Contains(7)) ||
+                (x.Contains(2) && x.Contains(5) && x.Contains(8)) ||
+                (x.Contains(3) && x.Contains(6) && x.Contains(9)) ||
+                (x.Contains(1) && x.Contains(5) && x.Contains(9)) ||
+                (x.Contains(3) && x.Contains(5) && x.Contains(7)))
+            {
+                this.XText.Text = "X Player Wins";
+                this.OText.Text = "O Player Loses";
+            }
+            else if ((o.Contains(1) && o.Contains(2) && o.Contains(3)) ||
+                (o.Contains(4) && o.Contains(5) && o.Contains(6)) ||
+                (o.Contains(7) && o.Contains(8) && o.Contains(9)) ||
+                (o.Contains(1) && o.Contains(4) && o.Contains(7)) ||
+                (o.Contains(2) && o.Contains(5) && o.Contains(8)) ||
+                (o.Contains(3) && o.Contains(6) && o.Contains(9)) ||
+                (o.Contains(1) && o.Contains(5) && o.Contains(9)) ||
+                (o.Contains(3) && o.Contains(5) && o.Contains(7)))
+            {
+                this.XText.Text = "X Player Loses";
+                this.OText.Text = "O Player Wins";
+            }
+
+        }
+
+
+
+        void OnClear(object sender, EventArgs e)
+        {
+            currentState = 1;
+            int ctr;
+
+            for(ctr = 0;ctr<10;ctr++)
+            {
+                
+
+            }
+
+            x.Clear();
+            o.Clear();
         }
     }
+
+
 }
+;
--- TicTacToe/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
+++ TicTacToe/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
@@ -8,21 +8,25 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: global::Xamarin.Forms.Xaml.XamlResourceIdAttribute("TicTacToe.MainPage.xaml", "MainPage.xaml", typeof(global::NewCalculator.MainPage))]
+[assembly: global::Xamarin.Forms.Xaml.XamlResourceIdAttribute("TicTacToe.MainPage.xaml", "MainPage.xaml", typeof(global::TicTacToe.MainPage))]
 
-namespace NewCalculator {
+namespace TicTacToe {
     
     
     [global::Xamarin.Forms.Xaml.XamlFilePathAttribute("MainPage.xaml")]
     public partial class MainPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
-        private global::Xamarin.Forms.Label resultText;
+        private global::Xamarin.Forms.Label XText;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Label OText;
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(MainPage));
-            resultText = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Label>(this, "resultText");
+            XText = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Label>(this, "XText");
+            OText = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Label>(this, "OText");
         }
     }
 }
